dbff0ae3f04e60ffcd7a2c16ba5f10f3f020dcf3
Philippe Luchansky <philuchansky@gmail.com>
2015-07-27 13:45:54 -0700

    generate snippets controller with all actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'snippets/index'
+
+  get 'snippets/show'
+
+  get 'snippets/new'
+
+  get 'snippets/create'
+
+  get 'snippets/edit'
+
+  get 'snippets/update'
+
+  get 'snippets/destroy'
+
   resources :users
 end
