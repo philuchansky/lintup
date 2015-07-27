113b7fe7f03446f9b749f4924896cd03fcffa954
Philippe Luchansky <philuchansky@gmail.com>
2015-07-27 16:28:01 -0700

    'users#show' with basic profile info.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,15 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
 
   end
 
   def create
-
-  end
-
-  def show
 
   end
 
