a8567f92ebdfcc0127b56174fba1397b984eb36f
Philippe Luchansky <philuchansky@gmail.com>
2015-07-27 11:29:27 -0700

    validation in User model rename User.github_url to User.github_username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727180915) do
+ActiveRecord::Schema.define(version: 20150727182327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20150727180915) do
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "github_url"
+    t.string   "github_username"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
