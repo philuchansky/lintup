a6bebde01b9f65f64e938c27d15356500ce37bd6
Philippe Luchansky <philuchansky@gmail.com>
2015-07-27 18:09:08 -0700

    New User form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,16 @@ class UsersController < ApplicationController
   end
 
   def new
-
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path @user
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -25,5 +30,10 @@ class UsersController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:fname,:lname,:username,:email,:password,:password_confirmation,:github_username)
   end
 end
