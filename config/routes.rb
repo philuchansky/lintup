b1d4f39be556764481898eecb8089da2a0bbd5fc
Philippe Luchansky <philuchansky@gmail.com>
2015-07-28 14:58:27 -0700

    snippet delete and delete confirmation routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/users/:id/confirm_delete' => 'users#destroy', as: :confirm_delete_user
   get '/snippets' => 'snippets#index_global'
   post '/users/:user_id/snippets/new' => 'snippets#create'
+  get '/users/:user_id/snippets/:id/delete' => 'snippets#confirm_destroy', as: :delete_snippet
+  get '/users/:user_id/snippets/:id/confirm_delete' => 'snippets#destroy', as: :confirm_delete_snippet
   resources :users, except: :destroy do
     resources :snippets
   end
@@ -13,5 +15,4 @@ Rails.application.routes.draw do
   ### SESSIONS ###
   get '/logout' => 'sessions#destroy'
   resources :sessions, only: [:new, :create, :destroy]
-
 end
