738b1e4218e9350053bc46b9e07872dd7140db3c
Philippe Luchansky <philuchansky@gmail.com>
2015-07-28 23:20:15 -0700

    install codemirror gem, and configure basic disabled text editor for snippets#show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require codemirror
+//= require codemirror/modes/ruby
+//= require codemirror/keymaps/sublime
 //= require_tree .
