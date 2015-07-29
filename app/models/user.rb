fc9610b01b4631cc969b630299d6b6f2b4a8eb52
Philippe Luchansky <philuchansky@gmail.com>
2015-07-28 18:22:24 -0700

    add destroy dependence to snippet and comment in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
 
-  has_many :snippets
-  has_many :comments
+  has_many :snippets, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
