0461d45b2b43a2efcabfcb36be369f2abf0dd8ab
Philippe Luchansky <philuchansky@gmail.com>
2015-07-31 10:02:05 -0700

    gravatar tooltip/link style tweaks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      flash[:success] = 'Welcome!'
       redirect_to user_path(@user)
     else
       redirect_to new_session_path
