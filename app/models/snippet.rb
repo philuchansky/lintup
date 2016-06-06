385646ba145e72037fe067bcdfededa4c526a33b
Philippe Luchansky <philuchansky@gmail.com>
2016-06-06 12:18:25 -0700

    update rails version, require presence of snippet title, language and content

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,5 +1,10 @@
 class Snippet < ActiveRecord::Base
   belongs_to :user
+
+  validates :title, presence: true
+  validates :language, presence: true
+  validates :content, presence: true
+
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
