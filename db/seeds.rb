6f44cdaa71fdb5a287947e07503dc46df88314f0
Philippe Luchansky <philuchansky@gmail.com>
2016-06-06 15:27:00 -0700

    populate seeds file with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@user1 = User.create({fname: "Jason", lname: "Lee", username: "jasonlee", email: "jason@gmail.com", password: "password", github_username: "dofjsdlfjsd"})
+@user2 = User.create({fname: "Shannon", lname: "Michaels", username: "shannonmichaels", email: "shannon@gmail.com", password: "password", github_username: "fljgndfljgndfg"})
+@user3 = User.create({fname: "Thor", lname: "Thorsen", username: "thor", email: "thor@gmail.com", password: "password", github_username: "sdofjsdlfjs"})
+@user4 = User.create({fname: "Jackie", lname: "Thomas", username: "jackie", email: "jackie@gmail.com", password: "password", github_username: "sodfjsodfjosdfj"})
+@user5 = User.create({fname: "Pat", lname: "Freedman", username: "pat", email: "pat@gmail.com", password: "password", github_username: "dosfjsodf"})
